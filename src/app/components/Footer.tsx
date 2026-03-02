7cc2c293c0f69385dc4fff42bec707080b014609
exelkonsol <exelkonsol@gmail.com>
2026-03-02 15:45:05 +0200

    update footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,6 @@
 ﻿import React from 'react';
 import { Twitter, Users, Activity, Link as LinkIcon } from 'lucide-react';
+import footerImage from '../../../images/footerimage.png';
 
 interface FooterProps {
   displayToast: (message: string) => void;
@@ -11,11 +12,11 @@ const Footer: React.FC<FooterProps> = ({ displayToast }) => {
     navigator.clipboard.writeText(ca).then(() => displayToast('CA COPIED'));
   };
 
-  const IMAGE_URL = 'https://raw.githubusercontent.com/exelkonsol/elk/main/images/newfooter.png';
+  const IMAGE_URL = footerImage;
 
   return (
     <footer
-      className="relative w-full overflow-hidden aspect-[3/2] md:aspect-auto md:h-[420px] lg:h-[500px] xl:h-[560px]"
+      className="relative w-full overflow-hidden aspect-[3/2] md:aspect-auto md:h-[340px] lg:h-[400px] xl:h-[460px]"
       style={{ background: '#000' }}
     >
       <div className="absolute inset-0 pointer-events-none select-none">
@@ -24,10 +25,10 @@ const Footer: React.FC<FooterProps> = ({ displayToast }) => {
           style={{
             backgroundImage: `url('${IMAGE_URL}')`,
             backgroundSize: 'cover',
-            backgroundPosition: 'center 38%',
+            backgroundPosition: 'center 58%',
             backgroundRepeat: 'no-repeat',
-            filter: 'saturate(1.02) brightness(0.92)',
-            opacity: 0.96,
+            filter: 'saturate(1.04) brightness(0.94)',
+            opacity: 0.98,
           }}
         />
         <div
